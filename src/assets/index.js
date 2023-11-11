f2ca7ef61e60c50a7291ac245e7fc8c28dd1c4f0
Nandar Sulistiawan <nandarsulistiawan21@mhs.unsiq.ac.id>
2023-11-11 15:45:20 +0700

    menambahkan BG LOGIN

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,19 +1,20 @@
-import LOGO_ALTARESTO from './images/logo_altaresto.png';
-import BG_ADMIN from './images/bg_admin.png';
-import DEFAULT_PROFILE_ADMIN from './images/default_profile_admin.png';
+import LOGO_ALTARESTO from "./images/logo_altaresto.png";
+import BG_ADMIN from "./images/bg_admin.png";
+import DEFAULT_PROFILE_ADMIN from "./images/default_profile_admin.png";
+import BG_LOGIN from "./images/bg_login.png";
 
-import DASHBOARD_ICON from './icons/dashboard_icon.svg';
-import DASHBOARD_ACTIVE_ICON from './icons/dashboard_active_icon.svg';
-import ANALISIS_ICON from './icons/analisis_icon.svg';
-import ANALISIS_ACTIVE_ICON from './icons/analisis_active_icon.svg';
-import PESANAN_ICON from './icons/pesanan_icon.svg';
-import PESANAN_ACTIVE_ICON from './icons/pesanan_active_icon.svg';
-import MENU_ICON from './icons/menu_icon.svg';
-import MENU_ACTIVE_ICON from './icons/menu_active_icon.svg';
-import RATING_ICON from './icons/rating_icon.svg';
-import RATING_ACTIVE_ICON from './icons/rating_active_icon.svg';
-import LOGOUT_ICON from './icons/logout_icon.svg';
-import LOGOUT_ACTIVE_ICON from './icons/logout_active_icon.svg';
+import DASHBOARD_ICON from "./icons/dashboard_icon.svg";
+import DASHBOARD_ACTIVE_ICON from "./icons/dashboard_active_icon.svg";
+import ANALISIS_ICON from "./icons/analisis_icon.svg";
+import ANALISIS_ACTIVE_ICON from "./icons/analisis_active_icon.svg";
+import PESANAN_ICON from "./icons/pesanan_icon.svg";
+import PESANAN_ACTIVE_ICON from "./icons/pesanan_active_icon.svg";
+import MENU_ICON from "./icons/menu_icon.svg";
+import MENU_ACTIVE_ICON from "./icons/menu_active_icon.svg";
+import RATING_ICON from "./icons/rating_icon.svg";
+import RATING_ACTIVE_ICON from "./icons/rating_active_icon.svg";
+import LOGOUT_ICON from "./icons/logout_icon.svg";
+import LOGOUT_ACTIVE_ICON from "./icons/logout_active_icon.svg";
 
 export {
   LOGO_ALTARESTO,
@@ -31,4 +32,5 @@ export {
   RATING_ACTIVE_ICON,
   LOGOUT_ICON,
   LOGOUT_ACTIVE_ICON,
+  BG_LOGIN,
 };
