9a7adf447ff4527a59f4a713f991b562c0eca907
toomiinn <butouchiha9@gmail.com>
2023-12-02 23:10:14 +0700

    (notifikasiPesan/feat): Create message notification

--- src/components/organisms/Topbar.jsx
+++ src/components/organisms/Topbar.jsx
@@ -1,6 +1,6 @@
-import { IoMdNotifications } from 'react-icons/io';
-import { MdEmail } from 'react-icons/md';
-import { DEFAULT_PROFILE_ADMIN } from '../../assets';
+import { MdEmail } from "react-icons/md";
+import { DEFAULT_PROFILE_ADMIN } from "../../assets";
+import NotifikasiPesan from "./NotifikasiPesan";
 
 export default function Topbar({ title }) {
   return (
@@ -8,7 +8,7 @@ export default function Topbar({ title }) {
       <h1 className="text-[30px] font-semibold">{title}</h1>
       <div className="flex items-center gap-10">
         <div className="flex items-center gap-12 text-brown">
-          <IoMdNotifications size={45} />
+          <NotifikasiPesan />
           <MdEmail size={45} />
         </div>
         <div className="flex items-center gap-5 border-l pl-10 border-gray-300">
@@ -16,11 +16,7 @@ export default function Topbar({ title }) {
             <p className="text-gray-500">Selamat datang</p>
             <h1 className="text-3xl font-medium">Admin</h1>
           </div>
-          <img
-            src={DEFAULT_PROFILE_ADMIN}
-            alt=""
-            className="w-20 h-20 rounded-full"
-          />
+          <img src={DEFAULT_PROFILE_ADMIN} alt="" className="w-20 h-20 rounded-full" />
         </div>
       </div>
     </div>
