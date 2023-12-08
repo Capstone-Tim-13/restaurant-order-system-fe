aa93a57fdf2f6beb41cc2e8701ea886d90f8e111
toomiinn <butouchiha9@gmail.com>
2023-12-09 02:36:52 +0700

    (notifikasiPesan/feat): Integrasi dengan mock API

--- src/components/organisms/NotifikasiPesan.jsx
+++ src/components/organisms/NotifikasiPesan.jsx
@@ -1,10 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { IoMdNotifications } from "react-icons/io";
 import { PESANAN_ICON, NOTIFICATION_ICON, LEFT_CIRCLE_ICON } from "../../assets";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default function NotifikasiPesan() {
   const [showDropdown, setShowDropdown] = useState(false);
+  const [notifications, setNotifications] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://6569e491de53105b0dd7d443.mockapi.io/api/dummy/pesanan");
+        const allNotifications = response.data;
+        const pendingNotifications = allNotifications.filter((notification) => notification.type === "pending");
+
+        const firstFourPendingNotifications = pendingNotifications.slice(0, 5);
+        setNotifications(firstFourPendingNotifications);
+      } catch (error) {
+        console.error("Fetching data error:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const toggleDropdown = () => {
     setShowDropdown(!showDropdown);
@@ -18,63 +37,29 @@ export default function NotifikasiPesan() {
 
       <div id="dropdownNotification" className={`absolute z-20 ${showDropdown ? "" : "hidden"} w-300 h-400 max-w-sm bg-white divide-y divide-gray-100 rounded-lg shadow border top-32 right-80`} aria-labelledby="dropdownNotificationButton">
         <div className="divide-y divide-gray-100">
-          <a href="#" className="flex px-4 py-3 hover:bg-gray-100 border-b ">
+          <a href="#" className="flex px-4 py-3 hover:bg-gray-100 border-b">
             <div className="flex-shrink-0 w-10 h-10 bg-amber-900 rounded-full flex items-center justify-center">
               <img className="" id="notifikasi" src={NOTIFICATION_ICON} />
             </div>
             <div className="w-full ps-3">
-              <div className="text-black text-sm mb-1.5">Terdapat 51 Order baru</div>
+              <div className="text-black text-sm mb-1.5">Terdapat {notifications.length} Order baru</div>
               <div className="text-black text-sm mb-1.5">menunggu konfirmasi</div>
             </div>
           </a>
 
-          <a href="#" className="flex px-4 py-3 hover:bg-gray-100">
-            <div className="flex-shrink-0 w-10 h-10 bg-amber-900 rounded-full flex items-center justify-center">
-              <img className="" id="pesanan" src={PESANAN_ICON} />
-            </div>
-            <div className="w-full ps-3">
-              <div className="text-black text-sm mb-1.5">Pesanan baru masuk order</div>
-              <div className="text-black text-sm mb-1.5">
-                ID <span className="text-yellow-800">#25334</span>
+          {notifications.map((notification) => (
+            <a href="#" key={notification.id} className="flex px-4 py-3 hover:bg-gray-100 border-b">
+              <div className="flex-shrink-0 w-10 h-10 bg-amber-900 rounded-full flex items-center justify-center">
+                <img className="" id="notifikasi" src={PESANAN_ICON} />
               </div>
-            </div>
-          </a>
-
-          <a href="#" className="flex px-4 py-3 hover:bg-gray-100">
-            <div className="flex-shrink-0 w-10 h-10 bg-amber-900 rounded-full flex items-center justify-center">
-              <img className="" id="pesanan" src={PESANAN_ICON} />
-            </div>
-            <div className="w-full ps-3">
-              <div className="text-black text-sm mb-1.5">Pesanan baru masuk order</div>
-              <div className="text-black text-sm mb-1.5">
-                ID <span className="text-yellow-800">#25334</span>
+              <div className="w-full ps-3">
+                <div className="text-black text-sm mb-1.5">Pesanan baru masuk order</div>
+                <div className="text-black text-sm mb-1.5">
+                  ID <span className="text-yellow-800">{notification.idOrder}</span>
+                </div>
               </div>
-            </div>
-          </a>
-
-          <a href="#" className="flex px-4 py-3 hover:bg-gray-100">
-            <div className="flex-shrink-0 w-10 h-10 bg-amber-900 rounded-full flex items-center justify-center">
-              <img className="" id="pesanan" src={PESANAN_ICON} />
-            </div>
-            <div className="w-full ps-3">
-              <div className="text-black text-sm mb-1.5">Pesanan baru masuk order</div>
-              <div className="text-black text-sm mb-1.5">
-                ID <span className="text-yellow-800">#25334</span>
-              </div>
-            </div>
-          </a>
-
-          <a href="#" className="flex px-4 py-3 hover:bg-gray-100">
-            <div className="flex-shrink-0 w-10 h-10 bg-amber-900 rounded-full flex items-center justify-center">
-              <img className="" id="pesanan" src={PESANAN_ICON} />
-            </div>
-            <div className="w-full ps-3">
-              <div className="text-black text-sm mb-1.5">Pesanan baru masuk order</div>
-              <div className="text-black text-sm mb-1.5">
-                ID <span className="text-yellow-800">#25334</span>
-              </div>
-            </div>
-          </a>
+            </a>
+          ))}
 
           <Link to="/admin/pesanan" className="flex items-center justify-between px-4 py-3 hover:bg-gray-100 pb-1.5">
             <div className="flex items-center justify-between">
