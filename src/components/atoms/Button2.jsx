6433d15f3e3d2fd3890a9d5045abdf3f6fcf49ac
akbaroke <akbarmuhammad833@gmail.com>
2023-12-11 16:06:08 +0700

    (development/refactor): add alert

--- src/components/atoms/Button2.jsx
+++ src/components/atoms/Button2.jsx
@@ -1,7 +1,15 @@
+import { notifyError } from './Toast';
+
 export default function ButtonMenu2() {
   return (
-    <div className="flex justify-end">
-      <button className="bg-amber-900 text-white font-bold py-4 px-16 rounded-full mb-4 mt-4 mr-32">Unggah File</button>
+    <div
+      className="flex justify-end"
+      onClick={() =>
+        notifyError('Maaf fitur masih dalam tahap pengembangan...')
+      }>
+      <button className="bg-amber-900 text-white font-bold py-4 px-16 rounded-full mb-4 mt-4 mr-32">
+        Unggah File
+      </button>
     </div>
   );
 }
--- src/pages/ConvertMenuPage.jsx
+++ src/pages/ConvertMenuPage.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import TextMenu from "../components/atoms/Text";
-import ButtonMenu from "../components/atoms/Button1";
-import UploadFile from "../components/organisms/FormUpload";
-import ButtonMenu2 from "../components/atoms/Button2";
-import { BACK_MENU } from "../assets";
+import TextMenu from '../components/atoms/Text';
+import ButtonMenu from '../components/atoms/Button1';
+import UploadFile from '../components/organisms/FormUpload';
+import ButtonMenu2 from '../components/atoms/Button2';
+import { BACK_MENU } from '../assets';
+import { Link } from 'react-router-dom';
 
 export default function MenuConvert() {
   return (
     <>
-      <div className="flex items-center">
+      <Link to="/admin/menu" className="flex items-center">
         <img className="" id="back-menu" src={BACK_MENU} />
         <h3 className="text-2xl font-semibold ml-5">Menu</h3>
-      </div>
+      </Link>
       <div className="bg-white p-8 rounded-3xl shadow-md max-w-full mx-auto mt-7">
         <TextMenu />
         <ButtonMenu />
